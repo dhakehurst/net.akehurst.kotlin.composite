2c31721d126f077f070b0724b86c4056c41b7ce0
David Akehurst <david.akehurst@itemis.de>
2019-11-24 15:57:43 +0100

    kotlin 1.3.60

--- net.akehurst.kotlin.komposite/komposite-processor/build.gradle.kts
+++ net.akehurst.kotlin.komposite/komposite-processor/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("net.akehurst.kotlin.kt2ts-plugin") version "1.0.0"
+}
+
 val version_agl:String by project
 val version_kotlinx:String by project
 
@@ -9,3 +13,25 @@ dependencies {
     commonMainImplementation("net.akehurst.kotlinx:kotlinx-reflect:$version_kotlinx")
 
 }
+
+val tsdDir ="${buildDir}/tmp/jsJar/ts"
+
+kotlin {
+    sourceSets {
+        val jsMain by getting {
+            resources.srcDir("${tsdDir}")
+        }
+    }
+}
+
+kt2ts {
+    outputDirectory.set(file(tsdDir))
+    localJvmName.set("jvm8")
+    modulesConfigurationName.set("jvm8RuntimeClasspath")
+    classPatterns.set(listOf(
+            "net.akehurst.kotlin.komposite.processor.*"
+    ))
+}
+
+tasks.getByName("generateTypescriptDefinitionFile").dependsOn("jvm8MainClasses")
+tasks.getByName("jsJar").dependsOn("generateTypescriptDefinitionFile")
